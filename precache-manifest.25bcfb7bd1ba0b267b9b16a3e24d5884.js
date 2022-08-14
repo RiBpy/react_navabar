0ed7bbe8796a650540cb357c091a77fe4afc8c8f
RiBpy <77004199+RiBpy@users.noreply.github.com>
2022-08-14 23:27:21 +0600

    Updates

---
+++ precache-manifest.25bcfb7bd1ba0b267b9b16a3e24d5884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b863e51548f3b2d6fc4033cbf63e7e9",
+    "url": "/react_navbar/index.html"
+  },
+  {
+    "revision": "6c82598dc58ab003ebee",
+    "url": "/react_navbar/static/css/main.d3056bba.chunk.css"
+  },
+  {
+    "revision": "a5f0ccb7e97bc84c9fa0",
+    "url": "/react_navbar/static/js/2.bf42b51c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_navbar/static/js/2.bf42b51c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c82598dc58ab003ebee",
+    "url": "/react_navbar/static/js/main.78abbb1f.chunk.js"
+  },
+  {
+    "revision": "072d217a6ad769c93e16",
+    "url": "/react_navbar/static/js/runtime-main.e5cd4e31.js"
+  }
+]);
